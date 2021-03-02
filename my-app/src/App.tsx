a4d0b95d3c48a6b26cc56960029ff97680e21147
김승완 <reddht721@gmail.comlspwdcdgit config --global user.email reddht721@gmail.com>
2021-03-02 19:31:20 +0900

    typescript redux

---
+++ my-app/src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css';
+import CounterCotainer from '../src/containers/CounterContainer';
+
+function App() {
+  return (
+    <div className="App">
+      <CounterCotainer />
+    </div>
+  );
+}
+
+export default App;
---
+++ my-app/src/modules/counter.ts
@@ -0,0 +1,66 @@
+// Action TYPE
+
+const INCREASE = 'counter/INCREASE' as const;
+const DECREASE = 'counter/DECREASE' as const;
+const INCREASE_BY = 'counter/INCREASE_BY' as const;
+
+// Action Creator
+export const increase = () => ({
+  type: INCREASE,
+});
+
+export const decrease = () => ({
+  type: DECREASE,
+});
+
+export const increaseBy = (diff: number) => ({
+  type: INCREASE_BY,
+  payload: diff,
+});
+
+// function increase() {
+//   return (type:INCREASE)
+// }
+
+// // View
+// increaseBy(5)
+
+// // Reducer
+// {
+//   type: INCREASE_BY,
+//   payload: 5,
+// }
+
+type CounterAction =
+  | ReturnType<typeof increase>
+  | ReturnType<typeof decrease>
+  | ReturnType<typeof increaseBy>;
+
+type CounterState = {
+  count: number;
+};
+
+const initialState: CounterState = {
+  count: 0,
+};
+
+// Reducer
+function counter(
+  state: CounterState = initialState,
+  action: CounterAction
+): CounterState {
+  switch (action.type) {
+    case INCREASE:
+      return { count: state.count + 1 };
+    case DECREASE:
+      return { count: state.count - 1 };
+    case INCREASE_BY:
+      return { count: state.count + action.payload };
+    default:
+      return state;
+  }
+}
+
+export default counter;
+
+// 리듀서는 state, action을 파라미터로 받는 함수이고 CounterState Type을 리턴하는 함수
